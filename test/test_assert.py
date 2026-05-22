b5fdc163d24531ca2702afb602805b17dc6651d8
Muhammad KK <muhammadibnuyoosuf@gmail.com>
2026-05-22 16:48:45 +0400

    multiple pytest ini and markers

--- test/test_assert.py
+++ test/test_assert.py
@@ -1,5 +1,26 @@
 import pytest
-
+@pytest.mark.smoke
+@pytest.mark.regression
+@pytest.mark.sanity
+def test_assert1():
+    assert 5==4+1,"value mismatch"
+    print("passed")
+@pytest.mark.regression
+@pytest.mark.sanity
+def test_assert2():
+    assert 5==4+1,"value mismatch"
+    print("passed")
+@pytest.mark.smoke
+@pytest.mark.regression
+def test_assert3():
+    assert 5==4+1,"value mismatch"
+    print("passed")
+@pytest.mark.sanity
+def test_assert4():
+    assert 5==4+1,"value mismatch"
+    print("passed")
+@pytest.mark.smoke
 def test_assert():
     assert 5==4+1,"value mismatch"
     print("passed")
+
--- test/test_google.py
+++ test/test_google.py
@@ -5,12 +5,39 @@ import allure
 
 
 #@pytest.mark.usefixtures("browser_launch")
-def test_google_title(driver):
+@pytest.mark.regression
+def test_google_title1(driver):
     driver.get("https://www.google.com")
-    assert "Yahoo" in driver.title
+    assert "Google" in driver.title
 
 #@pytest.mark.usefixtures("browser_launch")
-def test_automation(driver):
+@pytest.mark.regression
+def test_automation2(driver):
     driver.get("https://practicetestautomation.com/practice-test-login/")
-    assert "jenkins" in driver.title
+    assert "Login" in driver.title
 
+#@pytest.mark.usefixtures("browser_launch")
+@pytest.mark.smoke
+def test_google_title3(driver):
+    driver.get("https://www.google.com")
+    assert "Google" in driver.title
+
+#@pytest.mark.usefixtures("browser_launch")
+@pytest.mark.sanity
+def test_automation4(driver):
+    driver.get("https://practicetestautomation.com/practice-test-login/")
+    assert "Login" in driver.title
+
+#@pytest.mark.usefixtures("browser_launch")
+@pytest.mark.smoke
+@pytest.mark.regression
+@pytest.mark.sanity
+def test_google_title5(driver):
+    driver.get("https://www.google.com")
+    assert "Google" in driver.title
+
+#@pytest.mark.usefixtures("browser_launch")
+@pytest.mark.sanity
+def test_automation6(driver):
+    driver.get("https://practicetestautomation.com/practice-test-login/")
+    assert "Login" in driver.title
